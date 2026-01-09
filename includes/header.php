8b230589e82892dd1dfe1da8a4bc499a5a68ad22
Ade Reiki Karuna <adereiki02@gmail.com>
2026-01-09 08:19:06 +0700

    fix: add URL rewrite for clean URLs without pages folder

--- includes/header.php
+++ includes/header.php
@@ -56,12 +56,12 @@ require_once $basePath . 'config/config.php';
                     $pagesPath = $isInPages ? '' : 'pages/';
                     $navItems = [
                         ['url' => $homeUrl, 'label' => 'Beranda', 'icon' => 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6', 'file' => 'index.php'],
-                        ['url' => $basePath . $pagesPath . 'pegawai.php', 'label' => 'Pegawai', 'icon' => 'M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197m13.5-9a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0z', 'file' => 'pegawai.php'],
-                        ['url' => $basePath . $pagesPath . 'pelatihan.php', 'label' => 'Pelatihan', 'icon' => 'M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253', 'file' => 'pelatihan.php'],
-                        ['url' => $basePath . $pagesPath . 'jadwal.php', 'label' => 'Jadwal', 'icon' => 'M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z', 'file' => 'jadwal.php'],
-                        ['url' => $basePath . $pagesPath . 'kalender.php', 'label' => 'Kalender', 'icon' => 'M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z', 'file' => 'kalender.php'],
-                        ['url' => $basePath . $pagesPath . 'monitoring.php', 'label' => 'Monitoring', 'icon' => 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01', 'file' => 'monitoring.php'],
-                        ['url' => $basePath . $pagesPath . 'laporan.php', 'label' => 'Laporan', 'icon' => 'M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z', 'file' => 'laporan.php'],
+                        ['url' => $basePath . 'pegawai.php', 'label' => 'Pegawai', 'icon' => 'M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197m13.5-9a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0z', 'file' => 'pegawai.php'],
+                        ['url' => $basePath . 'pelatihan.php', 'label' => 'Pelatihan', 'icon' => 'M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253', 'file' => 'pelatihan.php'],
+                        ['url' => $basePath . 'jadwal.php', 'label' => 'Jadwal', 'icon' => 'M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z', 'file' => 'jadwal.php'],
+                        ['url' => $basePath . 'kalender.php', 'label' => 'Kalender', 'icon' => 'M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z', 'file' => 'kalender.php'],
+                        ['url' => $basePath . 'monitoring.php', 'label' => 'Monitoring', 'icon' => 'M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01', 'file' => 'monitoring.php'],
+                        ['url' => $basePath . 'laporan.php', 'label' => 'Laporan', 'icon' => 'M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z', 'file' => 'laporan.php'],
                     ];
                     foreach($navItems as $item):
                         $isActive = $currentPage == $item['file'];
